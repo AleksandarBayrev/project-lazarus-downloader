1db327bd1c3c191d9e047cd5079db3cf93b5ac7f
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2026-01-20 19:04:56 +0200

    Fix publish scripts

--- publish-linux-x64.sh
+++ publish-linux-x64.sh
@@ -2,6 +2,6 @@
 echo "Cleaning previous build artifacts..."
 rm -rfv ./bin
 rm -rfv ./obj
-echo "Publishing for linux-x64..."
+echo "Publishing for Linux x64..."
 dotnet publish -c Release -r linux-x64 --self-contained false
 echo "Publish completed."
--- publish-win-x64.sh
+++ publish-win-x64.sh
@@ -2,6 +2,6 @@
 echo "Cleaning previous build artifacts..."
 rm -rfv ./bin
 rm -rfv ./obj
-echo "Publishing for windows-x64..."
-dotnet publish -c Release -r windows-x64 --self-contained false
+echo "Publishing for Windows x64..."
+dotnet publish -c Release -r win-x64 --self-contained false
 echo "Publish completed."
